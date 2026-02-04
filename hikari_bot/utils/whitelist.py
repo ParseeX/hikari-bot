0052eca39c38d447013146425395e0c273b7f894
水橋パルスィ <xlightgod@gmail.com>
2026-02-05 00:22:02 +0900

    修改白名单读取逻辑

--- hikari_bot/utils/whitelist.py
+++ hikari_bot/utils/whitelist.py
@@ -6,7 +6,11 @@ from hikari_bot.utils.constants import *
 
 whitelist_file = os.path.join(DATA_DIR, 'whitelist.json')
 
-def get_whitelist():
+# 内存缓存
+_whitelist_cache = None
+
+def _load_whitelist_from_file():
+    """从文件加载白名单到内存"""
     try:
         with open(whitelist_file, 'r', encoding='utf-8') as f:
             return json.load(f)
@@ -14,12 +18,23 @@ def get_whitelist():
         return {"groups": [], "users": []}
     except json.JSONDecodeError:
         return {"groups": [], "users": []}
-    
+
+def get_whitelist():
+    """获取白名单（从内存缓存）"""
+    global _whitelist_cache
+    if _whitelist_cache is None:
+        _whitelist_cache = _load_whitelist_from_file()
+    return _whitelist_cache
+
 def save_whitelist(whitelist):
+    """保存白名单到文件并更新内存缓存"""
+    global _whitelist_cache
     with open(whitelist_file, 'w', encoding='utf-8') as f:
         json.dump(whitelist, f, indent=4, ensure_ascii=False)
+    _whitelist_cache = whitelist
 
 def add_group_to_whitelist(group_id):
+    """添加群组到白名单"""
     whitelist = get_whitelist()
     if group_id not in whitelist["groups"]:
         whitelist["groups"].append(group_id)
@@ -28,9 +43,11 @@ def add_group_to_whitelist(group_id):
     return False
 
 def is_allowed_group(group_id) -> bool:
+    """检查群组是否在白名单中"""
     return group_id in get_whitelist()["groups"]
     
 async def message_superusers(message: str):
+    """向所有超级用户发送消息"""
     try:
         bot = get_bot()
         for uid in get_driver().config.superusers:
